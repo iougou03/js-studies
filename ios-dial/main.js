96edf60d5b0e76db6e93e0907eee094996b1e357
iougou03 <haesol820@naver.com>
2022-12-05 13:02:03 +0900

    Implementing ios clock with circle round animation

--- ios-dial/main.js
+++ ios-dial/main.js
@@ -1,3 +1,12 @@
+// @ts-check
+
+/**
+ * 
+ * @param {Array<number>} arr 
+ */
+function sum(arr) {
+  return arr.reduce((acc, cur) => acc + cur,0);
+}
 function range(num) {
   return [...Array(num).keys()];
 }
@@ -10,42 +19,58 @@ const styles = `
     }
     #root {
         margin: auto;
-        max-width: 271px;
-				height: 135px;
+        width: 542px;
+				height: 271px;
 				background-color: #000;
 				display: flex;
 				align-items: center;
 				justify-content: center;
-        
+        position: relative;
+        padding: 0 20px;
     }
     .center-container {
 				width: 100%;	
         display: flex;
         background-color: #151516;
-        border-radius: 7px;
+        border-radius: 10px;
         justify-content: space-between;
-        padding: 2px 14px;
+        padding: 0 14px;
 				color: #fff;
 				font-size: 20px;
     }
 
     .dial-container {
-				width: 30px;
-				height: 30px;
 				position: relative;
+        cursor: pointer;
     }
     .dial {
         position: absolute;
 				top:0;
-				left:0;
-    }
-    li {
+				left:50%;
+        transform: translateX(-50%) translateZ(110px);
+      }
+      li {
         display: inline-block;
 				vertical-align: middle;
-				width: 30px;
-				height: 30px;
-				text-align: center;
-				
+				text-align: center; 
+    }
+
+    .shadow-up,
+    .shadow-bottom {
+      width: 100%;
+      height: 15%;
+      position: absolute;
+      z-index:10;
+    }
+    .shadow-up {
+      top:0;
+      left:0;
+      background: linear-gradient(180deg, #000000 30%, rgba(0, 0, 0, 0) 100%);
+    }
+    .shadow-bottom {
+      bottom: 0;
+      left: 0;
+      background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000 70%);
     }
 `;
 
@@ -56,19 +81,29 @@ class IosDial extends HTMLElement {
 
   maxValList = [10];
 
+  /**
+   * @type {Array<Array<number>>}
+   * @description 모든 li의 offsetTop 2차원 배열, dial index, num index로 접근
+   */
   coorPerDial = [];
 
+  /**
+   * @type {Array<{y: number, dest: number}>}
+   */
   positionPerDial = [];
 
   isKeydown = false;
 
   isMousedown = false;
   // mouseDest = 0;
-  mouseY = 0;
-  mouseOffsetTop = 0;
-  mouseDialInfo = { dialIdx: 0, numIdx: 0 }
+  prevMouseY = 0;
+  upperBound = 0;
+  underBound = 0;
+  mouseDialInfo = { dialIdx: 0 };
 
   dialElems = [];
+  rootElem = null;
+  centerElem = null;
 
   acc = 0.15;
   // for performance, since js calculate since position y is float number.
@@ -76,7 +111,6 @@ class IosDial extends HTMLElement {
 
   constructor() {
     super();
-
 
     this.attachShadow({ mode: "open" });
     const userInputCnt = this.getAttribute("cnt");
@@ -99,9 +133,9 @@ class IosDial extends HTMLElement {
   animate() {
     window.requestAnimationFrame(this.animate.bind(this));
 
-    if (this.isKeydow || this.isMousedown) return;
+    if (this.isKeydown || this.isMousedown) return;
 
-    // let 
+    // let
     // this.positionPerDial.forEach((posi) => {
     //   const md = Math.abs(-this.mouseDest - posi.y) * this.acc;
 
@@ -117,14 +151,14 @@ class IosDial extends HTMLElement {
 
     //   // console.log(posi.y, this.mouseDest)
     // });
-    
+
     // if (this.mouseDest !== 0) {
     //   this.drawDial();
     //   return;
     // }
 
     this.findSelfPosition();
-    
+
     this.drawDial();
     return;
   }
@@ -163,9 +197,36 @@ class IosDial extends HTMLElement {
     });
   }
 
+  /**
+   * 
+   * @param {number} totalLength
+   * @param {number} currentDest
+   * @param {number} totalIdx
+   * @param {number} idx
+   */
+  getDegree(totalLength, currentDest, totalIdx, idx) {
+    const percentage = currentDest / totalLength;
+    idx += 1;
+
+    return Math.round((90 / totalIdx) * percentage);
+  }
+
   drawDial(dialIdx) {
     this.positionPerDial.forEach(
-      (posi, idx) => (this.dialElems[idx].style.top = `${posi.y}px`)
+      (posi, idx) => {
+        this.dialElems[idx].style.top = `${posi.y}px`;
+
+        const totalLength = sum(this.coorPerDial[idx]);
+        const totalIdx = this.coorPerDial[idx].length;
+
+        const liElems = this.dialElems[idx].children;
+
+        [...liElems].forEach((li, numIdx) => {
+          const deg = this.getDegree(this.rootElem.offsetHeight, posi.y, totalIdx, numIdx);
+
+          li.style.transform = "rotateX(" + deg + "deg)";
+        })
+      }
     );
   }
 
@@ -174,14 +235,20 @@ class IosDial extends HTMLElement {
 
     // root width : 270.719 = font-size : 20
     const fontSize = (width * 20) / IPHONE_PPI_WIDTH;
-    // this.shadowRoot.querySelectorAll('li').forEach(elem => {
-    // 	elem.style.width=fontSize + "px"
-    // 	elem.style.height=fontSize + "px"
-    // })
+    
+    this.centerElem.style.fontSize = `${fontSize}px`;
+    
+    const h = this.centerElem.querySelector('li').offsetHeight;
+    
+    [...this.centerElem.querySelectorAll('li')].forEach(li => 
+      li.style.width = `${h}px`
+    )
+    const dcList = this.shadowRoot.querySelectorAll('.dial-container');
+    [...dcList].forEach(dc => {
+      dc.style.height=`${h}px`;
+      dc.style.width=`${h}px`;
+    })
 
-    this.shadowRoot.querySelector(
-      ".center-container"
-    ).style.fontSize = `${fontSize}px`;
   }
 
   keyDownHandler(e) {
@@ -201,34 +268,72 @@ class IosDial extends HTMLElement {
 
   mousedownHandler(e) {
     const target = e.composedPath()[0];
-    
-    if (target.classList.contains('dial-num')) {
-      const info = target.classList.item(0).split('-');
+
+    if (target.classList.contains("dial-num")) {
+      const info = target.classList.item(0).split("-");
+      const dialIdx = parseInt(info[1]);
+      const liLength = this.coorPerDial[dialIdx].length;
+
       this.isMousedown = true;
-      this.mouseOffsetTop = e.y;
+      this.prevMouseY = e.y;
+      this.upperBound = this.coorPerDial[dialIdx][0];
+      this.underBound = this.coorPerDial[dialIdx][liLength - 1];
       this.mouseDialInfo = {
-        dialIdx: parseInt(info[1]),
-      }
+        dialIdx,
+      };
     }
-    // this.mouseDest = e.y;
   }
 
   mouseupHandler(e) {
     this.isMousedown = false;
-    // this.mouseDest -= e.y;
+  }
+
+  /**
+   *
+   * @param {number} y
+   * @returns {{distance: number, direction: 'SCROLL_DOWN' | 'SCROLL_UP'}}
+   */
+  getMouseDirection(y) {
+    const distance = Math.abs(y - this.prevMouseY);
+    if (this.prevMouseY < y) {
+      return {
+        distance,
+        direction: "SCROLL_DOWN",
+      };
+    } else {
+      return {
+        distance,
+        direction: "SCROLL_UP",
+      };
+    }
   }
 
   mousemoveHandler(e) {
     if (this.isMousedown) {
-      const d = Math.abs(e.y - this.mouseOffsetTop) * this.acc;
-      // if (Math.abs(d) > 40) return;
+      const { distance, direction } = this.getMouseDirection(e.y);
+      this.prevMouseY = e.y;
 
-      console.log(e.y)
-      if (e.y < this.mouseOffsetTop) {
+      const maxDistance = this.rootElem.offsetHeight / 4;
+      const d = distance * 2;
+
+      if (direction === "SCROLL_UP") {
+        if (
+          this.positionPerDial[this.mouseDialInfo.dialIdx].y - d <
+          this.underBound - maxDistance
+        ) {
+          return;
+        }
         this.positionPerDial[this.mouseDialInfo.dialIdx].y -= d;
       } else {
+        if (
+          this.positionPerDial[this.mouseDialInfo.dialIdx].y + d >
+          this.upperBound + maxDistance
+        ) {
+          return;
+        }
         this.positionPerDial[this.mouseDialInfo.dialIdx].y += d;
       }
+
       this.drawDial();
     }
   }
@@ -262,39 +367,45 @@ class IosDial extends HTMLElement {
   }
 
   init() {
+
     this.dialElems = this.shadowRoot.querySelectorAll(".dial");
 
-    this.coorPerDial = [...this.dialElems].map((dial) =>
-      [...dial.children].map((li) => -li.offsetTop)
-    );
-    this.positionPerDial.forEach((posi, idx) => posi.y = this.coorPerDial[idx][0])
+    this.rootElem = this.shadowRoot.querySelector("#root");
 
-    this.drawDial()
+    this.centerElem = this.shadowRoot.querySelector(".center-container");
+
     this.attachObserver();
     this.resizeHandler();
+    
+    this.coorPerDial = [...this.dialElems].map((dial) =>
+    [...dial.children].map((li) => -li.offsetTop)
+    );
+    this.positionPerDial.forEach(
+      (posi, idx) => (posi.y = this.coorPerDial[idx][0])
+    );
   }
 
-  
   connectedCallback() {
     this.render();
     this.init();
-    
+    this.drawDial();
+
     window.addEventListener("resize", this.resizeHandler.bind(this));
     window.addEventListener("keydown", this.keyDownHandler.bind(this));
     window.addEventListener("keyup", this.keyUpHandler.bind(this));
-    
+
     window.addEventListener("mousedown", this.mousedownHandler.bind(this));
-    window.addEventListener('mousemove', this.mousemoveHandler.bind(this));
+    window.addEventListener("mousemove", this.mousemoveHandler.bind(this));
     window.addEventListener("mouseup", this.mouseupHandler.bind(this));
   }
-  
+
   disconnectedCallback() {
     window.removeEventListener("resize", this.resizeHandler);
     window.removeEventListener("keydown", this.keyDownHandler);
     window.removeEventListener("keyup", this.keyUpHandler);
 
     window.removeEventListener("mousedown", this.mousedownHandler);
-    window.removeEventListener('mousemove', this.mousemoveHandler);
+    window.removeEventListener("mousemove", this.mousemoveHandler);
     window.removeEventListener("mouseup", this.mouseupHandler);
   }
 
@@ -303,6 +414,7 @@ class IosDial extends HTMLElement {
 			<style>${styles}</style>
 			
 			<section id="root">
+          <div class="shadow-up"></div>
 
 					<div class="center-container">
 					${range(this.cnt)
@@ -323,6 +435,8 @@ class IosDial extends HTMLElement {
             )
             .join("")}
 					</div>
+
+          <div class="shadow-bottom"></div>
 			</section>
 		`;
   }
